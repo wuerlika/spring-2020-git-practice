2b4728268790dfe861f1f23fcfc6c6b36407e15c
wuerlika <Namu099052@gmail.com>
2020-07-22 23:39:28 -0400

    added practice1

---
+++ src/test/java/test/practice1.java
@@ -0,0 +1,7 @@
+package test;
+
+public class practice1 {
+    public static void main(String[] args) {
+        System.out.println("What do you looking for bro?!");
+    }
+}
---
+++ src/test/java/test/practice2.java
@@ -0,0 +1,7 @@
+package test;
+
+public class practice2 {
+    public static void main(String[] args) {
+        System.out.println(" looks like you very like my practice right? ");
+    }
+}
